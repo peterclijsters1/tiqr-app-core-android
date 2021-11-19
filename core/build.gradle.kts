628d7f9b038824b858fd3a0665cd2c418b2126bb
shachem <sara.hachem@withings.com>
2021-11-19 12:13:02 +0100

    test echo

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -196,10 +196,7 @@ publishing {
     }
 
     signing {
-        val signingKeyId: String? by project
-        val signingKey: String? by project
-        val signingPassword: String? by project
-        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+        useGpgCmd()
         sign(publishing.publications["mavenAndroid"])
     }
 }
