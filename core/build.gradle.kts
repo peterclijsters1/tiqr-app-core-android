480b414079c0729fb9dd07913859bce3939abade
dima-ko <dima@egeniq.com>
2021-11-25 15:07:51 +0200

    Added SHANPSHOT

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -116,7 +116,7 @@ val secureProperties = loadCustomProperties(file("../local.properties"))
 }
 
 group = "org.tiqr"
-version = "0.0.19"
+version = "0.0.19-SHANPSHOT"
 
 tasks {
     register("sourcesJar", Jar::class) {
