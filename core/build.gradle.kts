27770917227a7f64e6291263a94c413b98b648e3
dima-ko <dima@egeniq.com>
2021-11-25 17:51:12 +0200

    v0.0.21-SNAPSHOT

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -116,7 +116,7 @@ val secureProperties = loadCustomProperties(file("../local.properties"))
 }
 
 group = "org.tiqr"
-version = "0.0.21"
+version = "0.0.21-SNAPSHOT"
 
 tasks {
     register("sourcesJar", Jar::class) {
