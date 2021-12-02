ffb1997e7e71b6c63c6f4a3f2954df9daf504fe2
dima-ko <dima@egeniq.com>
2021-12-02 17:46:39 +0200

    Tryfix for data dependency issue all modules

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -119,9 +119,9 @@ group = "org.tiqr"
 
 version =
     if (System.getenv("IS_RELEASE") == "true") {
-        System.getenv("NEXT_VERSION").substring(0, System.getenv("NEXT_VERSION").indexOf("-"))
+        System.getenv("CORE_VERSION").substring(0, System.getenv("CORE_VERSION").indexOf("-"))
     } else {
-        System.getenv("CURRENT_VERSION").replace("-BETA-", ".").plus("-SNAPSHOT")
+        System.getenv("CORE_VERSION").replace("-BETA-", ".").plus("-SNAPSHOT")
     }
 
 tasks {
