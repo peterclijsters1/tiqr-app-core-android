df2310a2b15d52454e297db6894dc87beaea16ea
Wiebe Elsinga <w.elsinga@gmail.com>
2022-09-02 15:49:39 +0200

    no message

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
         classpath(libs.dagger.hilt.gradle)
         classpath(libs.androidx.navigation.gradle)
         classpath(libs.google.gms.gradle)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     }
 }
 
